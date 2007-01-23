dad467386062865ce10d69e9098551c0f5eccbd7
Heiko Hund <heikoh@users.sf.net>
2007-01-23 18:41:21 +0000

    removed border from w3c image

--- web/validate.php
+++ web/validate.php
@@ -31,7 +31,7 @@ else {
 	<img src="http://sflogo.sourceforge.net/sflogo.php?group_id=184171&amp;type=1" width="88" height="31" border="0" alt="SourceForge.net Logo" />
 </a>
 <a href="http://validator.w3.org/check?uri=referer">
-	<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" />
+	<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" border="0" />
 </a>
 </body>
 </html>
