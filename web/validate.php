999a6dc6d32f9053349a966faccbb4b78082f18c
Heiko Hund <heikoh@users.sf.net>
2007-03-28 07:02:38 +0000

    fixed link button

--- web/validate.php
+++ web/validate.php
@@ -37,11 +37,11 @@ else {
 
 <p>
 <a href="http://sourceforge.net/projects/libsieve-php/">
-	<img src="http://sflogo.sourceforge.net/sflogo.php?group_id=184171&amp;type=1" width="88" height="31" border="0" alt="SourceForge.net Logo" />
-</a>
+	<img src="http://sflogo.sourceforge.net/sflogo.php?group_id=184171&amp;type=1"
+	     width="88" height="31" border="0" alt="SourceForge.net Logo" /></a>
 <a href="http://validator.w3.org/check?uri=referer">
-	<img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Transitional" height="31" width="88" border="0" />
-</a>
+	<img src="http://www.w3.org/Icons/valid-xhtml10" height="31" width="88"
+	     border="0" alt="Valid XHTML 1.0 Transitional" /></a>
 </p>
 </div>
 </body>
