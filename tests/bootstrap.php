bc104dfe331d00ec4932fbb61c62cf7cb39b9857
Thomas Hareau <thomas.hareau@protonmail.com>
2019-11-26 20:47:54 +0100

    fix tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 // Settings to make all errors more obvious during testing
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', (string) 1);
+ini_set('display_startup_errors', (string) 1);
 date_default_timezone_set('UTC');
